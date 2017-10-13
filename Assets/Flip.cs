de82b4148c69a8cac2a88039a83b81ce1f019531
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-10-12 18:02:29 -0700

    At a point in time

---
+++ Assets/Flip.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace TabletopCardCompanion
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Flip : NetworkBehaviour
+    {
+        private SpriteRenderer spriteRenderer;
+        private NetworkIdentity networkIdentity;
+        private bool isFlipped;
+
+        private delegate void ParameterlessMethod();
+        private ParameterlessMethod callbackCommand;
+
+        private void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            networkIdentity = GetComponent<NetworkIdentity>();
+        }
+
+        public override void OnStartAuthority()
+        {
+            base.OnStartAuthority();
+            if (callbackCommand != null && hasAuthority)
+            {
+//                callbackCommand();
+                CmdFlip();
+                callbackCommand = null;
+                Player.Singleton.CmdReleaseOwnership(networkIdentity);
+            }
+        }
+
+        private void OnMouseOver()
+        {
+            if (Input.GetButtonDown("Vertical"))
+            {
+                callbackCommand = CmdFlip;
+                Player.Singleton.CmdRequestOwnership(networkIdentity);
+            }
+        }
+
+        [Command]
+        private void CmdFlip()
+        {
+            RpcFlip();
+        }
+
+        [ClientRpc]
+        private void RpcFlip()
+        {
+            spriteRenderer.color = isFlipped ? Color.white : Color.red;
+            isFlipped = !isFlipped;
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace TabletopCardCompanion
+{
+    public class Player : NetworkBehaviour
+    {
+        public static Player Singleton { get; private set; }
+
+        public override void OnStartLocalPlayer()
+        {
+            base.OnStartLocalPlayer();
+            Singleton = this;
+        }
+
+        [Command]
+        public void CmdRequestOwnership(NetworkIdentity identity)
+        {
+            identity.AssignClientAuthority(connectionToClient);
+        }
+
+        [Command]
+        public void CmdReleaseOwnership(NetworkIdentity identity)
+        {
+            identity.RemoveClientAuthority(connectionToClient);
+        }
+    }
+}
